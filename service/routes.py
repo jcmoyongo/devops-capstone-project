af07ec2fbfc2bdbdf001472d3a73b6d59e9a544d
Tino Moyongo <tino.moyongo@gmail.com>
2022-11-30 17:32:10 -0500

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -92,6 +92,7 @@ def get_accounts(account_id):
 
     return account.serialize(), status.HTTP_200_OK
 
+
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
@@ -111,6 +112,7 @@ def update_accounts(account_id):
     account.update()
 
     return account.serialize(), status.HTTP_200_OK
+
 
 ######################################################################
 # DELETE AN ACCOUNT
